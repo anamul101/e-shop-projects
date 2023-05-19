71e0dfb740109d5952c19e185d895fe78d3a8b6b
anamul101 <rjanamul01871@gmail.com>
2023-05-19 21:26:55 +0100

    shop profile

--- frontend/src/components/Shop/Layout/ShopProfileData.jsx
+++ frontend/src/components/Shop/Layout/ShopProfileData.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
 import styles from '../../../styles/styles';
-import { productData } from '../../../static/data';
+// import { productData } from '../../../static/data';
 import ProductCard from '../../Route/ProductCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProductsShop } from '../../../redux/actions/product';
 
 const ShopProfileData = ({ isOwner }) => {
+  const { products } = useSelector((state) => state.products);
+  const { events } = useSelector((state) => state.events);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getAllProductsShop(id));
+    // dispatch(getAllEventsShop(id));
+  },[dispatch])
   const [active, setActive] = useState(1);
   return (
     <div className='w-full'>
@@ -52,8 +63,8 @@ const ShopProfileData = ({ isOwner }) => {
       <br />
       {active === 1 && (
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
-          {productData &&
-            productData.map((i, index) => (
+          {products &&
+            products.map((i, index) => (
               <ProductCard data={i} key={index} isShop={true} />
             ))}
         </div>
