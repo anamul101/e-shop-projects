e0d86e574f3914d4fb908a8feb7c3e450dfc545d
anamul101 <rjanamul01871@gmail.com>
2023-05-25 19:54:43 +0100

    change password

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -314,4 +314,39 @@ router.delete(
   })
 );
 
+// update user password
+router.put(
+  "/update-user-password",
+  isAuthenticated,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const user = await User.findById(req.user.id).select("+password");
+
+      const isPasswordMatched = await user.comparePassword(
+        req.body.oldPassword
+      );
+
+      if (!isPasswordMatched) {
+        return next(new ErrorHandler("Old password is incorrect!", 400));
+      }
+
+      if (req.body.newPassword !== req.body.confirmPassword) {
+        return next(
+          new ErrorHandler("Password doesn't matched with each other!", 400)
+        );
+      }
+      user.password = req.body.newPassword;
+
+      await user.save();
+
+      res.status(200).json({
+        success: true,
+        message: "Password updated successfully!",
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
 module.exports = router;
--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -283,10 +283,79 @@ const TrackOrder = () => {
     )
 }
 const ChangePassword = () => {
-    return (
-        <div>
+    const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
+  const passwordChangeHandler = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .put(
+        `${server}/user/update-user-password`,
+        { oldPassword, newPassword, confirmPassword },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        toast.success(res.data.success);
+        setOldPassword("");
+        setNewPassword("");
+        setConfirmPassword("");
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
+    return (
+        <div className="w-full px-5">
+        <h1 className="block text-[25px] text-center font-[600] text-[#000000ba] pb-2">
+          Change Password
+        </h1>
+        <div className="w-full">
+          <form
+            aria-required
+            onSubmit={passwordChangeHandler}
+            className="flex flex-col items-center"
+          >
+            <div className=" w-[100%] 800px:w-[50%] mt-5">
+              <label className="block pb-2">Enter your old password</label>
+              <input
+                type="password"
+                className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+                required
+                value={oldPassword}
+                onChange={(e) => setOldPassword(e.target.value)}
+              />
+            </div>
+            <div className=" w-[100%] 800px:w-[50%] mt-2">
+              <label className="block pb-2">Enter your new password</label>
+              <input
+                type="password"
+                className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+                required
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+            </div>
+            <div className=" w-[100%] 800px:w-[50%] mt-2">
+              <label className="block pb-2">Enter your confirm password</label>
+              <input
+                type="password"
+                className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+                required
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <input
+                className={`w-[95%] h-[40px] border border-[#3a24db] text-center text-[#3a24db] rounded-[3px] mt-8 cursor-pointer`}
+                required
+                value="Update"
+                type="submit"
+              />
+            </div>
+          </form>
         </div>
+      </div>
     )
 }
 const Address = () => {
