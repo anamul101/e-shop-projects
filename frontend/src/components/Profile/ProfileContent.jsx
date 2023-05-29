b4ca34255eda5301534dea5f736bcd3726c9a0b0
anamul101 <rjanamul01871@gmail.com>
2023-05-29 20:18:30 +0100

    order button add

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -208,34 +208,30 @@ const AllOrders = () => {
                         {/* head */}
                         <thead>
                             <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Job</th>
-                                <th>Favorite Color</th>
+                                <th>Order ID</th>
+                                <th>Status</th>
+                                <th>Items Qty</th>
+                                <th>Total</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {/* row 1 */}
-                            <tr>
-                                <th>1</th>
-                                <td>Cy Ganderton</td>
-                                <td>Quality Control Specialist</td>
-                                <td>Blue</td>
-                            </tr>
-                            {/* row 2 */}
-                            <tr>
-                                <th>2</th>
-                                <td>Hart Hagerty</td>
-                                <td>Desktop Support Technician</td>
-                                <td>Purple</td>
-                            </tr>
-                            {/* row 3 */}
-                            <tr>
-                                <th>3</th>
-                                <td>Brice Swyre</td>
-                                <td>Tax Accountant</td>
-                                <td>Red</td>
-                            </tr>
+                            {
+                                orders && orders.map((singleOrder)=>
+                                <tr>
+                                    <th>{singleOrder._id}</th>
+                                    <td><p className="text-green-600">{singleOrder.status}</p></td>
+                                    <td>{singleOrder.cart.length}</td>
+                                    <td>USD${singleOrder.totalPrice}</td>
+                                    <td>
+                                        <button className="hover:background-gray-400">
+                                        <Link to={`/user/order/${singleOrder._id}`}>
+                                            <AiOutlineArrowRight size={20}/>
+                                        </Link>
+                                        </button>
+                                    </td>
+                                </tr>
+                                )
+                            }
                         </tbody>
                     </table>
                 </div>
