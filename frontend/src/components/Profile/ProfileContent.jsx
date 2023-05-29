5f4937fe6899355e8f5f3bc36c7544d2a49eb20a
anamul101 <rjanamul01871@gmail.com>
2023-05-29 18:57:11 +0100

    order table add

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -12,9 +12,10 @@ import { RxCross1 } from "react-icons/rx";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { deleteUserAddress, loadUser, updatUserAddress, updateUserInformation } from "../../redux/actions/user";
-import {toast} from "react-hot-toast"
+import { toast } from "react-hot-toast"
 import axios from "axios";
-import {Country,State} from "country-state-city"
+import { Country, State } from "country-state-city"
+import { getAllOrdersOfUser } from "../../redux/actions/order";
 
 const ProfileContent = ({ active }) => {
     const { user, error, successMessage } = useSelector((state) => state.user);
@@ -27,43 +28,43 @@ const ProfileContent = ({ active }) => {
 
     useEffect(() => {
         if (error) {
-          toast.error(error);
-          dispatch({ type: "clearErrors" });
+            toast.error(error);
+            dispatch({ type: "clearErrors" });
         }
         if (successMessage) {
-          toast.success(successMessage);
-          dispatch({ type: "clearMessages" });
+            toast.success(successMessage);
+            dispatch({ type: "clearMessages" });
         }
-      }, [error, successMessage]);
-    
+    }, [error, successMessage]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(updateUserInformation( name,email,phoneNumber, password));
+        dispatch(updateUserInformation(name, email, phoneNumber, password));
     }
     const handleImage = async (e) => {
         const file = e.target.files[0];
         setAvatar(file);
-    
+
         const formData = new FormData();
-    
+
         formData.append("image", e.target.files[0]);
-    
+
         await axios
-          .put(`${server}/user/update-avatar`, formData, {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-            withCredentials: true,
-          })
-          .then((response) => {
-            //  dispatch(loadUser());
-            //  toast.success("avatar updated successfully!");
-            window.location.reload();
-          })
-          .catch((error) => {
-            toast.error(error);
-          });
-    
+            .put(`${server}/user/update-avatar`, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                withCredentials: true,
+            })
+            .then((response) => {
+                //  dispatch(loadUser());
+                //  toast.success("avatar updated successfully!");
+                window.location.reload();
+            })
+            .catch((error) => {
+                toast.error(error);
+            });
+
     }
     return (
         <div className="w-full">
@@ -191,81 +192,55 @@ const ProfileContent = ({ active }) => {
 };
 
 const AllOrders = () => {
-    // const { orders } = useSelector((state) => state.order);
-    // const columns = [
-    //     { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+    const { user } = useSelector((state) => state.user);
+    const { orders } = useSelector((state) => state.order);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getAllOrdersOfUser(user._id))
+    }, [])
 
-    //     {
-    //         field: "status",
-    //         headerName: "Status",
-    //         minWidth: 130,
-    //         flex: 0.7,
-    //         cellClassName: (params) => {
-    //             return params.getValue(params.id, "status") === "Delivered"
-    //                 ? "greenColor"
-    //                 : "redColor";
-    //         },
-    //     },
-    //     {
-    //         field: "itemsQty",
-    //         headerName: "Items Qty",
-    //         type: "number",
-    //         minWidth: 130,
-    //         flex: 0.7,
-    //     },
-
-    //     {
-    //         field: "total",
-    //         headerName: "Total",
-    //         type: "number",
-    //         minWidth: 130,
-    //         flex: 0.8,
-    //     },
-
-    //     {
-    //         field: " ",
-    //         flex: 1,
-    //         minWidth: 150,
-    //         headerName: "",
-    //         type: "number",
-    //         sortable: false,
-    //         renderCell: (params) => {
-    //             return (
-    //                 <>
-    //                     <Link to={`/user/order/${params.id}`}>
-    //                         <button>
-    //                             <AiOutlineArrowRight size={20} />
-    //                         </button>
-    //                     </Link>
-    //                 </>
-    //             );
-    //         },
-    //     },
-    // ];
-
-    // const row = [];
-
-    // orders &&
-    //     orders.forEach((item) => {
-    //         row.push({
-    //             id: item._id,
-    //             itemsQty: item.cart.length,
-    //             total: "US$ " + item.totalPrice,
-    //             status: item.status,
-    //         });
-    //     });
 
     return (
-        <div className="pl-8 pt-1">
-            {/* <DataGrid
-                rows={row}
-                columns={columns}
-                pageSize={10}
-                autoHeight
-                disableSelectionOnClick
-            /> */}
-            Cooming Soon
-        </div>
+        <>
+            <div className="pl-8 pt-1">
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>
+                            {/* row 2 */}
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Desktop Support Technician</td>
+                                <td>Purple</td>
+                            </tr>
+                            {/* row 3 */}
+                            <tr>
+                                <th>3</th>
+                                <td>Brice Swyre</td>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
     )
 }
 const AllRefundOrders = () => {
@@ -284,78 +259,78 @@ const TrackOrder = () => {
 }
 const ChangePassword = () => {
     const [oldPassword, setOldPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-  const passwordChangeHandler = async (e) => {
-    e.preventDefault();
+    const passwordChangeHandler = async (e) => {
+        e.preventDefault();
 
-    await axios
-      .put(
-        `${server}/user/update-user-password`,
-        { oldPassword, newPassword, confirmPassword },
-        { withCredentials: true }
-      )
-      .then((res) => {
-        toast.success(res.data.success);
-        setOldPassword("");
-        setNewPassword("");
-        setConfirmPassword("");
-      })
-      .catch((error) => {
-        toast.error(error.response.data.message);
-      });
-  };
+        await axios
+            .put(
+                `${server}/user/update-user-password`,
+                { oldPassword, newPassword, confirmPassword },
+                { withCredentials: true }
+            )
+            .then((res) => {
+                toast.success(res.data.success);
+                setOldPassword("");
+                setNewPassword("");
+                setConfirmPassword("");
+            })
+            .catch((error) => {
+                toast.error(error.response.data.message);
+            });
+    };
     return (
         <div className="w-full px-5">
-        <h1 className="block text-[25px] text-center font-[600] text-[#000000ba] pb-2">
-          Change Password
-        </h1>
-        <div className="w-full">
-          <form
-            aria-required
-            onSubmit={passwordChangeHandler}
-            className="flex flex-col items-center"
-          >
-            <div className=" w-[100%] 800px:w-[50%] mt-5">
-              <label className="block pb-2">Enter your old password</label>
-              <input
-                type="password"
-                className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
-                required
-                value={oldPassword}
-                onChange={(e) => setOldPassword(e.target.value)}
-              />
+            <h1 className="block text-[25px] text-center font-[600] text-[#000000ba] pb-2">
+                Change Password
+            </h1>
+            <div className="w-full">
+                <form
+                    aria-required
+                    onSubmit={passwordChangeHandler}
+                    className="flex flex-col items-center"
+                >
+                    <div className=" w-[100%] 800px:w-[50%] mt-5">
+                        <label className="block pb-2">Enter your old password</label>
+                        <input
+                            type="password"
+                            className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+                            required
+                            value={oldPassword}
+                            onChange={(e) => setOldPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className=" w-[100%] 800px:w-[50%] mt-2">
+                        <label className="block pb-2">Enter your new password</label>
+                        <input
+                            type="password"
+                            className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+                            required
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className=" w-[100%] 800px:w-[50%] mt-2">
+                        <label className="block pb-2">Enter your confirm password</label>
+                        <input
+                            type="password"
+                            className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
+                            required
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                        <input
+                            className={`w-[95%] h-[40px] border border-[#3a24db] text-center text-[#3a24db] rounded-[3px] mt-8 cursor-pointer`}
+                            required
+                            value="Update"
+                            type="submit"
+                        />
+                    </div>
+                </form>
             </div>
-            <div className=" w-[100%] 800px:w-[50%] mt-2">
-              <label className="block pb-2">Enter your new password</label>
-              <input
-                type="password"
-                className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
-                required
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-              />
-            </div>
-            <div className=" w-[100%] 800px:w-[50%] mt-2">
-              <label className="block pb-2">Enter your confirm password</label>
-              <input
-                type="password"
-                className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
-                required
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-              <input
-                className={`w-[95%] h-[40px] border border-[#3a24db] text-center text-[#3a24db] rounded-[3px] mt-8 cursor-pointer`}
-                required
-                value="Update"
-                type="submit"
-              />
-            </div>
-          </form>
         </div>
-      </div>
     )
 }
 const Address = () => {
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -5,7 +5,7 @@ import { productReducer } from "./reducers/product";
 import { eventReducer } from "./reducers/event";
 import { cartReducer } from "./reducers/cart";
 import { wishlistReducer } from "./reducers/wishlist";
-// import { orderReducer } from "./reducers/order";
+import { orderReducer } from "./reducers/order";
 
 
 const Store = configureStore({
@@ -16,7 +16,7 @@ const Store = configureStore({
     events: eventReducer,
     cart: cartReducer,
     wishlist: wishlistReducer,
-    // order: orderReducer,
+    order: orderReducer,
   },
 });
 
