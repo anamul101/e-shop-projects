dba1116ec2a168b26940adb4000fc3ef8d4d1f5c
anamul101 <rjanamul01871@gmail.com>
2023-06-06 06:01:58 +0100

    review part

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -7,6 +7,7 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const Shop = require("../model/shop");
 const { isSeller, isAuthenticated, isAdmin } = require("../middleware/auth");
 const fs = require("fs");
+const Order = require("../model/order");
 
 // create product
 router.post(
@@ -102,6 +103,63 @@ router.get(
       res.status(201).json({
         success: true,
         products,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
+
+// review for a product
+router.put(
+  "/create-new-review",
+  isAuthenticated,
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const { user, rating, comment, productId, orderId } = req.body;
+
+      const product = await Product.findById(productId);
+
+      const review = {
+        user,
+        rating,
+        comment,
+        productId,
+      };
+
+      const isReviewed = product.reviews.find(
+        (rev) => rev.user._id === req.user._id
+      );
+
+      if (isReviewed) {
+        product.reviews.forEach((rev) => {
+          if (rev.user._id === req.user._id) {
+            (rev.rating = rating), (rev.comment = comment), (rev.user = user);
+          }
+        });
+      } else {
+        product.reviews.push(review);
+      }
+
+      let avg = 0;
+
+      product.reviews.forEach((rev) => {
+        avg += rev.rating;
+      });
+
+      product.ratings = avg / product.reviews.length;
+
+      await product.save({ validateBeforeSave: false });
+
+      await Order.findByIdAndUpdate(
+        orderId,
+        { $set: { "cart.$[elem].isReviewed": true } },
+        { arrayFilters: [{ "elem._id": productId }], new: true }
+      );
+
+      res.status(200).json({
+        success: true,
+        message: "Reviwed succesfully!",
       });
     } catch (error) {
       return next(new ErrorHandler(error, 400));
