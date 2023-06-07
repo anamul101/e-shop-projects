f69749f95f8535152b7908601d356e15d0f9c804
anamul101 <rjanamul01871@gmail.com>
2023-06-07 20:57:32 +0100

    shop profile date

--- frontend/src/components/Shop/Layout/ShopProfileData.jsx
+++ frontend/src/components/Shop/Layout/ShopProfileData.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import styles from '../../../styles/styles';
-// import { productData } from '../../../static/data';
 import ProductCard from '../../Route/ProductCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProductsShop } from '../../../redux/actions/product';
+import { getAllEventsShop } from '../../../redux/actions/event';
+import { backend_url } from '../../../server';
+import Ratings from '../../Products/Ratings';
 
 const ShopProfileData = ({ isOwner }) => {
   const { products } = useSelector((state) => state.products);
@@ -14,9 +16,10 @@ const ShopProfileData = ({ isOwner }) => {
 
   useEffect(()=>{
     dispatch(getAllProductsShop(id));
-    // dispatch(getAllEventsShop(id));
+    dispatch(getAllEventsShop(id));
   },[dispatch])
   const [active, setActive] = useState(1);
+  const allReviews =products && products.map((product) => product.reviews).flat();
   return (
     <div className='w-full'>
       <div className="flex w-full items-center justify-between">
@@ -67,6 +70,53 @@ const ShopProfileData = ({ isOwner }) => {
             products.map((i, index) => (
               <ProductCard data={i} key={index} isShop={true} />
             ))}
+        </div>
+      )}
+      {active === 2 && (
+        <div className="w-full">
+          <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] xl:grid-cols-4 xl:gap-[20px] mb-12 border-0">
+            {events &&
+              events.map((i, index) => (
+                <ProductCard
+                  data={i}
+                  key={index}
+                  isShop={true}
+                  isEvent={true}
+                />
+              ))}
+          </div>
+          {events && events.length === 0 && (
+            <h5 className="w-full text-center py-5 text-[18px]">
+              No Events have for this shop!
+            </h5>
+          )}
+        </div>
+      )}
+       {active === 3 && (
+        <div className="w-full">
+          {allReviews &&
+            allReviews.map((item, index) => (
+              <div className="w-full flex my-4">
+                <img
+                  src={`${backend_url}/${item.user.avatar}`}
+                  className="w-[50px] h-[50px] rounded-full"
+                  alt=""
+                />
+                <div className="pl-2">
+                  <div className="flex w-full items-center">
+                    <h1 className="font-[600] pr-2">{item.user.name}</h1>
+                    <Ratings rating={item.rating} />
+                  </div>
+                  <p className="font-[400] text-[#000000a7]">{item?.comment}</p>
+                  <p className="text-[#000000a7] text-[14px]">{"2days ago"}</p>
+                </div>
+              </div>
+            ))}
+          {allReviews && allReviews.length === 0 && (
+            <h5 className="w-full text-center py-5 text-[18px]">
+              No Reviews have for this shop!
+            </h5>
+          )}
         </div>
       )}
     </div>
