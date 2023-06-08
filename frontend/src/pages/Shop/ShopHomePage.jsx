529ffbf32f5a3251a8e437b9dd51ef6f84b2346d
anamul101 <rjanamul01871@gmail.com>
2023-06-08 18:27:14 +0100

    change

--- frontend/src/pages/Shop/ShopHomePage.jsx
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import styles from '../../styles/styles'
 import ShopInfo from "../../components/Shop/ShopInfo";
 import ShopProfileData from '../../components/Shop/Layout/ShopProfileData';
-import Footer from '../../components/Layout/Footer';
-import Header from '../../components/Layout/Header';
 
 const ShopHomePage = () => {
   return (
-    <div>
-      <Header />
       <div className={`${styles.section} bg-[#f5f5f5]`}>
         <div className="w-full flex py-10 justify-between">
           <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
@@ -19,8 +15,6 @@ const ShopHomePage = () => {
           </div>
         </div>
       </div>
-      <Footer />
-    </div>
   )
 }
 
