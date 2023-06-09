07f4a72ddcdf1c9872d3604176ba72617a10e1dc
anamul101 <rjanamul01871@gmail.com>
2023-06-09 15:37:28 +0100

    messages

--- backend/app.js
+++ backend/app.js
@@ -32,9 +32,11 @@ const coupon = require("./controller/coupounCode");
 const payment = require("./controller/payment");
 const order = require("./controller/order");
 const conversation = require("./controller/conversation");
+const message = require("./controller/message");
 
 app.use("/api/v2/user", user);
 app.use("/api/v2/conversation", conversation);
+app.use("/api/v2/message", message);
 app.use("/api/v2/shop", shop);
 app.use("/api/v2/product", product);
 app.use("/api/v2/event", event);
