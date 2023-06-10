0ff9360738481aa66b18766981843b5476d2d639
anamul101 <rjanamul01871@gmail.com>
2023-06-10 09:55:34 +0100

    messages socket

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -312,7 +312,7 @@ const MessageList = ({
         <p className="text-[16px] text-[#000c]">
           {data?.lastMessageId !== user?._id
             ? "You:"
-            : user?.name.split(" ")[0] + ": "}{" "}
+            : user?.name?.split(" ")[0] + ": "}{" "}
           {data?.lastMessage}
         </p>
       </div>
