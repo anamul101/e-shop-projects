7d979bbefaeb1b9c41b5f4c30e85976bde832bd5
anamul101 <rjanamul01871@gmail.com>
2023-06-13 20:32:28 +0100

    users delete

---
+++ frontend/src/components/Admin/AllUsers.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllUsers } from "../../redux/actions/user";
+import { AiOutlineDelete } from "react-icons/ai";
+import styles from "../../styles/styles";
+import { RxCross1 } from "react-icons/rx";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-hot-toast";
+
+const AllUsers = () => {
+    const dispatch = useDispatch();
+    const { users } = useSelector((state) => state.user);
+    const [open, setOpen] = useState(false);
+    const [userId, setUserId] = useState("");
+
+    useEffect(() => {
+        dispatch(getAllUsers());
+    }, [dispatch]);
+
+    const handleDelete = async (id) => {
+        await axios
+            .delete(`${server}/user/delete-user/${id}`, { withCredentials: true })
+            .then((res) => {
+                toast.success(res.data.message);
+            });
+
+        dispatch(getAllUsers());
+    };
+
+    return (
+        <div className="w-full flex justify-center pt-5">
+            <div className="w-[97%]">
+                <h3 className="text-[22px] font-Poppins pb-2">All Users</h3>
+                <div className="w-full min-h-[45vh] bg-white rounded">
+                <div className="overflow-x-auto">
+                        <table className="table w-full">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Role</th>
+                                    <th> joinedAt</th>
+                                    <th> Delete User</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    users && users.map((item) =>
+                                        <tr>
+                                            <th>{item._id}</th>
+                                            <td><p>{item.name}</p></td>
+                                            <td>{item.email}</td>
+                                            <td>{item.role}</td>
+                                            <td>{item.createdAt.slice(0, 10)}</td>
+                                            <td>
+                                                <button
+                                                    onClick={() => setUserId(item._id) || setOpen(true)}
+                                                >
+                                                    <AiOutlineDelete size={20} />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            {open && (
+                <div className="w-full fixed top-0 left-0 z-[999] bg-[#00000039] flex items-center justify-center h-screen">
+                    <div className="w-[95%] 800px:w-[40%] min-h-[20vh] bg-white rounded shadow p-5">
+                        <div className="w-full flex justify-end cursor-pointer">
+                            <RxCross1 size={25} onClick={() => setOpen(false)} />
+                        </div>
+                        <h3 className="text-[25px] text-center py-5 font-Poppins text-[#000000cb]">
+                            Are you sure you wanna delete this user?
+                        </h3>
+                        <div className="w-full flex items-center justify-center">
+                            <div
+                                className={`${styles.button} text-white text-[18px] !h-[42px] mr-4`}
+                                onClick={() => setOpen(false)}
+                            >
+                                cancel
+                            </div>
+                            <div
+                                className={`${styles.button} text-white text-[18px] !h-[42px] ml-4`}
+                                onClick={() => setOpen(false) || handleDelete(userId)}
+                            >
+                                confirm
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+  );
+};
+
+export default AllUsers;
