b721173c38f901ad929bf3ed0029af040e332657
anamul101 <rjanamul01871@gmail.com>
2023-06-13 19:07:38 +0100

    all sellers

--- frontend/src/components/Admin/AdminDashboardMain.jsx
+++ frontend/src/components/Admin/AdminDashboardMain.jsx
@@ -59,7 +59,7 @@ const AdminDashboardMain = () => {
                 All Sellers
               </h3>
             </div>
-            {/* <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{sellers && sellers.length}</h5> */}
+            <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{sellers && sellers.length}</h5>
             <Link to="/admin-sellers">
               <h5 className="pt-4 pl-2 text-[#077f9c]">View Sellers</h5>
             </Link>
@@ -106,7 +106,7 @@ const AdminDashboardMain = () => {
                                         <th>{singleOrder._id}</th>
                                         <td><p className="text-green-600">{singleOrder.status}</p></td>
                                         <td>{singleOrder.cart.length}</td>
-                                        <td>USD${singleOrder.totalPrice}</td>
+                                        <td>${singleOrder.totalPrice}</td>
                                         <td>
                                             <button className="hover:background-gray-400">
                                                 <Link to={`/order/${singleOrder._id}`}>
