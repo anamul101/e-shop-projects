28c2a1e1df2b81335704d22f0807dceae05d7555
anamul101 <rjanamul01871@gmail.com>
2023-06-17 17:18:49 +0100

    best deal ubdate

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -4,35 +4,35 @@ import styles from '../../styles/styles'
 import EventCard from './EventCard';
 
 const Events = () => {
-      const {allEvents,isLoading} = useSelector((state) => state.events);
-      
+    const { allEvents, isLoading } = useSelector((state) => state.events);
+
 
     return (
         <div>
             {
                 !isLoading && (
                     <div className={`${styles.section}`}>
-                <div className={`${styles.heading}`}>
-                    <h1>Popular Events</h1>
-                </div>
+                        <div className={`${styles.heading}`}>
+                            <h1>Popular Events</h1>
+                        </div>
 
-                <div className="w-full grid">
-                {
-          allEvents.length !== 0 && (
-            <EventCard data={allEvents && allEvents[0]} />
-          )
-         }
-         <h4>{
-           allEvents?.length === 0 && (
-            'No Events have!'
-           )
-          }
+                        <div className="w-full grid">
+                            {
+                                allEvents.length !== 0 && (
+                                    <EventCard data={allEvents && allEvents[0]} />
+                                )
+                            }
+                            <h4 className="text-2xl text-red-500 py-11 text-center">{
+                                allEvents?.length === 0 && (
+                                    'No Events have!'
+                                )
+                            }
 
-         </h4>
+                            </h4>
 
-                </div>
+                        </div>
 
-            </div>
+                    </div>
                 )
             }
         </div>
