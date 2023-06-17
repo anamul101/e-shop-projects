e5981121d8289b1d91053bb478dc0496eb3c23a5
anamul101 <rjanamul01871@gmail.com>
2023-06-17 12:32:44 +0100

    events ubdate

--- frontend/src/components/Events/CountDown.jsx
+++ frontend/src/components/Events/CountDown.jsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { server } from "../../server";
 
 const CountDown = ({data}) => {
     const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
@@ -7,6 +9,15 @@ const CountDown = ({data}) => {
         const timer = setTimeout(() => {
             setTimeLeft(calculateTimeLeft());
         }, 1000);
+        if (
+            typeof timeLeft.days === 'undefined' &&
+            typeof timeLeft.hours === 'undefined' &&
+            typeof timeLeft.minutes === 'undefined' &&
+            typeof timeLeft.seconds === 'undefined'
+          ) {
+            axios.delete(`${server}/event/delete-shop-event/${data._id}`);
+          }
+      
         return () => clearTimeout(timer);
     });
 
