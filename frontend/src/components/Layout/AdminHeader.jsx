9fe1de6ea4421d0367099d173034249d00e93f8b
anamul101 <rjanamul01871@gmail.com>
2023-06-21 19:27:06 +0100

    admin header ubdates

--- frontend/src/components/Layout/AdminHeader.jsx
+++ frontend/src/components/Layout/AdminHeader.jsx
@@ -6,12 +6,16 @@ import { MdOutlineLocalOffer } from 'react-icons/md'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { backend_url } from '../../server'
+import { BsHandbag } from 'react-icons/bs'
+import { GrWorkshop } from 'react-icons/gr'
+import { HiOutlineUserGroup } from 'react-icons/hi'
+import { CiMoneyBill } from 'react-icons/ci'
 
 const AdminHeader = () => {
-    const {user} = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.user);
 
   return (
-         <div className="w-full h-[80px] bg-[#054349] shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
+    <div className="w-full h-[80px] bg-[#054349] shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
       <div>
         <Link to="/">
           <img
@@ -22,42 +26,51 @@ const AdminHeader = () => {
       </div>
       <div className="flex items-center">
         <div className="flex items-center mr-4">
-          <Link to="/dashboard/cupouns" className="800px:block hidden">
-            <AiOutlineGift
-              color="#fff"
-              size={30}
-              className="mx-5 cursor-pointer"
-            />
-          </Link>
-          <Link to="/dashboard-events" className="800px:block hidden">
+          <Link to="/admin-events" className="800px:block hidden">
             <MdOutlineLocalOffer
               color="#fff"
               size={30}
               className="mx-5 cursor-pointer"
             />
           </Link>
-          <Link to="/dashboard-products" className="800px:block hidden">
+          <Link to="/admin-products" className="800px:block hidden">
+            <BsHandbag
+              color="#fff"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to="/admin-orders" className="800px:block hidden">
             <FiShoppingBag
               color="#fff"
               size={30}
               className="mx-5 cursor-pointer"
             />
           </Link>
-          <Link to="/dashboard-orders" className="800px:block hidden">
-            <FiPackage color="#fff" size={30} className="mx-5 cursor-pointer" />
-          </Link>
-          <Link to="/dashboard-messages" className="800px:block hidden">
-            <BiMessageSquareDetail
+          <Link to="/admin-withdraw-request" className="800px:block hidden">
+            <CiMoneyBill
               color="#fff"
               size={30}
               className="mx-5 cursor-pointer"
             />
           </Link>
+          <Link to="/admin-sellers" className="800px:block hidden">
+            <GrWorkshop color="#fff" size={30} className="mx-5 cursor-pointer" />
+          </Link>
+          <Link to="/admin-users" className="800px:block hidden">
+            <HiOutlineUserGroup
+              color="#fff"
+              size={30}
+              className="mx-5 cursor-pointer"
+            />
+          </Link>
+          <Link to="/profile">
             <img
               src={`${backend_url}${user?.avatar}`}
               alt=""
               className="w-[50px] h-[50px] rounded-full object-cover"
             />
+          </Link>
         </div>
       </div>
     </div>
