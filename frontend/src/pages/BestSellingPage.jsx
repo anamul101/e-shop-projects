0d07e244c57dcd78cd5f5fc6b4852ebe210a17ab
anamul101 <rjanamul01871@gmail.com>
2023-06-22 21:27:17 +0100

    showroute update

--- frontend/src/pages/BestSellingPage.jsx
+++ frontend/src/pages/BestSellingPage.jsx
@@ -5,6 +5,7 @@ import Loader from "../components/Layout/Loader";
 import ProductCard from "../components/Route/ProductCard";
 import styles from "../styles/styles";
 import Footer from "../components/Layout/Footer";
+import ShowRoute from "../components/Sheard/ShowRoute";
 
 const BestSellingPage = () => {
   const [data, setData] = useState([]);
@@ -24,9 +25,11 @@ const BestSellingPage = () => {
     ) : (
       <div>
       <Header activeHeading={2} />
+      <ShowRoute title="All Best Selling" first="best-selling"></ShowRoute>
       <br />
       <br />
       <div className={`${styles.section}`}>
+      <h2 className="text-3xl font-bold text-gray-900 mb-8">Best Selling</h2>
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
           {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
         </div>
