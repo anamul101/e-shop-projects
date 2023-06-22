a004c63c3dd009ecd6d8af869320d3cb91cd3157
anamul101 <rjanamul01871@gmail.com>
2023-06-22 21:05:20 +0100

    products page update

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -6,6 +6,7 @@ import Header from "../components/Layout/Header";
 import Loader from "../components/Layout/Loader";
 import ProductCard from "../components/Route/ProductCard";
 import styles from "../styles/styles";
+import ShowRoute from "../components/Sheard/ShowRoute";
 
 const ProductsPage = () => {
     const [searchParams] = useSearchParams();
@@ -33,14 +34,16 @@ const ProductsPage = () => {
                 ) : (
                     <div>
                         <Header activeHeading={3} />
+                        <ShowRoute title="All Products" first="products"></ShowRoute>
                         <br />
                         <br />
                         <div className={`${styles.section}`}>
+                        <h2 className="text-3xl font-bold text-gray-900 mb-8">Products</h2>
                             <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
                                 {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
                             </div>
                             {data && data.length === 0 ? (
-                                <h1 className="text-center w-full pb-[100px] text-[20px]">
+                                <h1 className="text-center text-red-500 w-full pb-[100px] text-[20px]">
                                     No products Found!
                                 </h1>
                             ) : null}
