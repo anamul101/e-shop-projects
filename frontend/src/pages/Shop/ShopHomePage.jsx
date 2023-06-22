6f1897983ef8f2d5cec2b1984534de4b7a327f92
anamul101 <rjanamul01871@gmail.com>
2023-06-22 12:12:25 +0100

    previewpage ubdate

--- frontend/src/pages/Shop/ShopHomePage.jsx
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from '../../styles/styles'
 import ShopInfo from "../../components/Shop/ShopInfo";
 import ShopProfileData from '../../components/Shop/Layout/ShopProfileData';
-import Header from '../../components/Layout/Header';
+import DashboardHeader from '../../components/Shop/Layout/DashboardHeader';
 
 const ShopHomePage = () => {
   return (
+     <>
+     <DashboardHeader/>
       <div className={`${styles.section} bg-[#f5f5f5]`}>
-        <Header/>
         <div className="w-full 800px:flex py-10 justify-between">
           <div className="800px:w-[25%] bg-[#fff] rounded-[4px] shadow-sm 800px:overflow-y-scroll 800px:h-[90vh] 800px:sticky top-10 left-0 z-10">
             <ShopInfo isOwner={true} />
@@ -17,6 +18,7 @@ const ShopHomePage = () => {
           </div>
         </div>
       </div>
+     </>
   )
 }
 
--- frontend/src/pages/Shop/ShopPreviewPage.jsx
+++ frontend/src/pages/Shop/ShopPreviewPage.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import styles from '../../styles/styles'
 import ShopInfo from "../../components/Shop/ShopInfo";
 import ShopProfileData from '../../components/Shop/Layout/ShopProfileData';
+import DashboardHeader from '../../components/Shop/Layout/DashboardHeader';
 
 const ShopPreviewPage = () => {
   return (
+    <>
+    <DashboardHeader/>
     <div className={`${styles.section} bg-[#f5f5f5]`}>
          <div className="w-full 800px:flex py-10 justify-between">
           <div className="800px:w-[25%] bg-[#fff] rounded-[4px] shadow-sm 800px:overflow-y-scroll 800px:h-[90vh] 800px:sticky top-10 left-0 z-10">
@@ -15,6 +18,7 @@ const ShopPreviewPage = () => {
           </div>
          </div>
     </div>
+    </>
   )
 }
 
