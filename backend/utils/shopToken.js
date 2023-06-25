fac7a0b68410d3d48592dc1256a20ec486380311
anamul101 <rjanamul01871@gmail.com>
2023-06-25 12:15:39 +0100

    deployment

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -6,7 +6,8 @@ const sendShopToken = (user, statusCode, res) => {
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      Secure: true,
+      sameSite:"none",
+      secure: true,
     };
   
     res.status(statusCode).cookie("seller_token", token, options).json({
