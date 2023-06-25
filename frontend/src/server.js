c52a89625048281c205e93c0b256583ef6f24c81
anamul101 <rjanamul01871@gmail.com>
2023-06-25 19:01:02 +0100

    server page done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
 export const server = "https://e-shop-projects.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://e-shop-projects.vercel.app/";
