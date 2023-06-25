6b0713970d412785c0712543b6a7e4cdc15807d1
anamul101 <rjanamul01871@gmail.com>
2023-06-25 11:35:19 +0100

    ubdate11

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:"https://e-shop-projects.vercel.app/",
   credentials:true,
 }));
 app.use("/", express.static("uploads"));
